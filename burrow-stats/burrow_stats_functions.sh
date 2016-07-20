032955af0b7e42fc08ff2bde213a7255adf8be8e
Peter Doyle <peter@Peters-MBP.hsd1.co.comcast.net>
2016-07-19 19:28:49 -0600

    burrow-stats functions are provided to setup the configs.json interactively before building the docker image

---
+++ burrow-stats/burrow_stats_functions.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+segment_buider() {
+
+  cg_name=$1
+  burrow_host=$2
+  burrow_port=$3
+  topic_name=$4
+  cluster_name=$5
+
+echo { \
+  \"name\": \"$cg_name\", \
+  \"status\": \"http://$burrow_host:$burrow_port/v2/kafka/$cluster_name/consumer/$cg_name/status\", \
+  \"consumer_group_offset\": \"http://$burrow_host:$burrow_port/v2/kafka/$cluster_name/consumer/$cg_name/topic/$topic_name\", \
+  \"topic_offset\": \"http://$burrow_host:$burrow_port/v2/kafka/$cluster_name/topic/$topic_name\" \
+}
+}
+
+build_segments() {
+
+  define_another='y'
+  count=1
+  cat configs.orig.json > configs.json
+  while [ $define_another = 'y' ]; do
+    # get details
+    read -e -p "Enter the consumer group name: " -i "consumer-group-1" cg_name
+    read -e -p "Enter the burrow host name: " -i "localhost" burrow_host
+    read -e -p "Enter the burrow port: " -i "8082" burrow_port
+    read -e -p "Enter the topic name: " -i "use-case-1" topic_name
+    read -e -p "Enter the cluster name: " -i "local" cluster_name
+
+    # build the segment
+    segments="$(segment_buider $cg_name $burrow_host $burrow_port $topic_name $cluster_name)"
+
+    # insert into the configs.json file
+    sed -i "/\],/i $segments" configs.json
+
+    read -e -p "Define another consumer group segment (y/n)?: " -i "n" define_another
+    count=$((count+1))
+    if [ $define_another == 'y' ]; then
+      if [ $count -gt 1 ]; then
+        # add a comma between segments
+        sed -i "/\],/i \," configs.json
+      fi
+    fi
+
+  done
+
+}
--- burrow-stats/docker_build.sh
+++ burrow-stats/docker_build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+. ./burrow_stats_functions.sh
 
 no_cache=$1
 
@@ -6,6 +7,8 @@ if [ ! -d 'burrow-stats' ]; then
   git clone https://github.com/tulios/burrow-stats
 fi
 
+build_segments
+
 cp -vf configs.json burrow-stats/configs.json
 
-docker build $no_cache -t="streamworks/burrow-stats" burrow-stats/
+# docker build $no_cache -t="streamworks/burrow-stats" burrow-stats/
