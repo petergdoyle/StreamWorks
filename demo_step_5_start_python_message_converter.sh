670782c77fedcbab1823948c0c03b317feca6763
Peter Doyle <peter@Peters-MBP.hsd1.co.comcast.net>
2016-08-05 06:10:11 -0600

    enhanched the script to start or stop or run the python_message_converter container

--- demo_step_5_start_python_message_converter.sh
+++ demo_step_5_start_python_message_converter.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+container_name='streamworks_python'
 
-python-message-converter/docker_run.sh
+container_built=$(docker ps -a|grep $container_name |awk 'NF>1{print $NF}')
+if [ ! "$container_built" == $container_name ]; then
+  python-message-converter/docker_run.sh
+else
+  container_running=$(docker ps |grep $container_name |awk 'NF>1{print $NF}')
+  if [ "$container_running" == $container_name ]; then
+    docker start $container_name
+  fi
+fi
 sleep 3
 python-message-converter/docker_tailf_logs.sh
