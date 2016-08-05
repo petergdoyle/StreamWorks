41d45e0242014d8dfcdffb60d536b0ccac9032d1
Peter Doyle <peter@Peters-MBP.hsd1.co.comcast.net>
2016-08-05 09:59:57 -0600

    updated graphics

--- demo_step_0_reset_all.sh
+++ demo_step_0_reset_all.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 # remove project containers
-for each in $(docker ps -a|grep $project_name |awk 'NF>1{print $NF}'); do
+for each in $(docker ps -a|grep 'streamworks_'|awk 'NF>1{print $NF}'); do
   cmd="docker stop $each"
+  echo $cmd
+  eval "$cmd"
+done
+
+# remove project containers
+for each in $(docker ps -a|grep 'streamworks_'|awk 'NF>1{print $NF}'); do
+  cmd="docker start $each"
   echo $cmd
   eval "$cmd"
 done
