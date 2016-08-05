a071e352623ba217927dca33812f7b7d3abbcddd
Peter Doyle <peter@Peters-MBP.hsd1.co.comcast.net>
2016-08-05 05:16:23 -0600

    changed the default message rate down to 2tps and allow for it to be controlled by a variable in the script

--- external/streamworks_run_message_sender.sh
+++ external/streamworks_run_message_sender.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd $(dirname $0)
-
-java -Xms1G -Xmx1G -DLoadGenerator.KafkaMessageSenderBuilder.broker_url="localhost:9092" -DLoadGenerator.KafkaMessageSenderBuilder.topic_name="splash_csv" -DLoadGenerator.AirlineDataPayloadGeneratorBuilder.airlineDataFormatterType="com.cleverfishsoftware.loadgenerator.payload.air.AirlineDataFormatterCSV" -cp .:target/MessageSender-1.0-SNAPSHOT.jar com.cleverfishsoftware.loadgenerator.MessageSenderRunner com.cleverfishsoftware.loadgenerator.sender.kafka.KafkaMessageSenderBuilder com.cleverfishsoftware.loadgenerator.payload.air.AirlineDataPayloadGeneratorBuilder 4 0 0 2 n
+message_rate=2
+java -Xms1G -Xmx1G -DLoadGenerator.KafkaMessageSenderBuilder.broker_url="localhost:9092" -DLoadGenerator.KafkaMessageSenderBuilder.topic_name="splash_csv" -DLoadGenerator.AirlineDataPayloadGeneratorBuilder.airlineDataFormatterType="com.cleverfishsoftware.loadgenerator.payload.air.AirlineDataFormatterCSV" -cp .:target/MessageSender-1.0-SNAPSHOT.jar com.cleverfishsoftware.loadgenerator.MessageSenderRunner com.cleverfishsoftware.loadgenerator.sender.kafka.KafkaMessageSenderBuilder com.cleverfishsoftware.loadgenerator.payload.air.AirlineDataPayloadGeneratorBuilder $message_rate 0 0 2 n
