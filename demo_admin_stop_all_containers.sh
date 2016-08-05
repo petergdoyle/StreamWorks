6304645ff552f03408af8ebfbb137ec3bc669ffe
Peter Doyle <peter@Peters-MBP.hsd1.co.comcast.net>
2016-08-05 05:46:43 -0600

    demo script cleanup naming and functionality

---
+++ demo_admin_stop_all_containers.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+project_name='streamworks'
+
+# stop project containers
+for each in $(docker ps |grep $project_name |awk 'NF>1{print $NF}'); do
+  cmd="docker stop $each"
+  echo $cmd
+  eval "$cmd"
+done
--- demo_step_1_start_kafka.sh
+++ demo_step_1_start_kafka.sh
@@ -1,1 +1,5 @@
 #!/bin/sh
+
+docker start streamworks_kafka_zk
+sleep 2
+docker start streamworks_kafka_broker
